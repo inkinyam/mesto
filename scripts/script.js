c46ff37cb451505f5ca1e4ba5217e8c5b6331976
Natalya Serebrennikova <inkinyam@yandex.ru>
2022-03-16 21:35:09 +0300

    feat: сделать открытие большой фото, плавное открытие попапов

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,6 @@ const photoPopup       = document.querySelector('.popup-photo')
 const editForm         = document.querySelector('.popup-edit__wrapper');
 const addForm          = document.querySelector('.popup-add__wrapper');
 
-
 //Заполнение попапа editPopup
 const title            = document.querySelector('.profile__title');
 const subtitle         = document.querySelector('.profile__subtitle');
@@ -17,9 +16,8 @@ const editButton       = document.querySelector('.profile__edit-button');
 const addButton        = document.querySelector('.profile__add-button');
 
 // Кпнопки закрытия попапов
-const editExitButton   = document.querySelector('.popup-edit__exit');
-const addExitButton    = document.querySelector('.popup-add__exit');
-const photoExitButton  = document.querySelector('.popup-photo__exit');
+const editPopupExitButton   = document.querySelector('.popup-edit__exit');
+const addPopupExitButton    = document.querySelector('.popup-add__exit');
 
 // Кнопки сохранения попапов
 const addCardButton    = document.querySelector('.popup-add__save');
@@ -28,31 +26,31 @@ const addCardButton    = document.querySelector('.popup-add__save');
 const places            = document.querySelector('.places');
 
 // Начальный массив карточек
-const initialCards     = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+const initialCards = [
+  {
+    name: 'Крым',
+    link: 'https://images.unsplash.com/photo-1597181227326-0742677fe67c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Рим',
+    link: 'https://images.unsplash.com/photo-1638707743406-3e2a940252b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+  },
+  {
+    name: 'Каппадокия',
+    link: 'https://fregataero.ru/images/turkey/Cappadocia/cappadocia-805624_960_720.jpg'
+  },
+  {
+    name: 'Москва',
+    link: 'https://images.unsplash.com/photo-1598516443513-109b9e3f5294?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Стокгольм',
+    link: 'https://images.unsplash.com/photo-1553538435-cb923cf2d82a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://images.unsplash.com/photo-1614093576028-920b30d65326?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
+  }
 ];
 
 function openPopup(popup){
@@ -68,6 +66,7 @@ function closePopup(popup){
   popup.classList.remove('popup_opened');
 }
 
+// Функция сохранения на форму редактирования профиля
 function editFormSubmitHandler (event) {
   event.preventDefault();
   title.textContent = inputTitle.value;
@@ -77,22 +76,20 @@ function editFormSubmitHandler (event) {
 
 editForm.addEventListener('submit', editFormSubmitHandler);
 
-
+// Функция сохранения на форму добавления
 function addFormSubmitHandler (event) {
   event.preventDefault();
   const newCard = {
-      name: '',
-      link: ''}
+                    name: '',
+                    link: ''}
   let cardName = document.querySelector('.popup__item_el_name');
   let cardLink = document.querySelector('.popup__item_el_link');
   newCard.name = cardName.value;
   newCard.link = cardLink.value;
 
   if (places.children.length === 6) {
-
-    places.lastElementChild.remove();
+      places.lastElementChild.remove();
   }
-
   renderCard (newCard);
   closePopup(addPopup);
   cardName.value='';
@@ -114,27 +111,39 @@ addButton.addEventListener('click', function () {
 
 
 // Вызовы функций закрытия попапов
-editExitButton.addEventListener('click', function () {
+editPopupExitButton.addEventListener('click', function () {
   closePopup(editPopup);
 });
 
-addExitButton.addEventListener('click', function () {
+addPopupExitButton.addEventListener('click', function () {
   closePopup(addPopup);
 });
 
-photoExitButton.addEventListener('click', function () {
-  closePopup(photoPopup);
-});
+//Создаем и открываем заполненный попап с фоткой
+function createPopup (link, name) {
+  const popupTemplate  = document.querySelector('#popup-photo').content;
+  const popupElement   = popupTemplate.querySelector('.popup__figure').cloneNode(true);
 
+  popupElement.querySelector('.popup__image').src = link;
+  popupElement.querySelector('.popup__image').alt = name;
+  popupElement.querySelector('.popup__caption').textContent = name;
+  popupElement.querySelector('.popup-photo__exit').addEventListener ('click', function (evt) {
+    closePopup(photoPopup);
+    evt.target.parentElement.remove();
+  });
+
+  photoPopup.prepend(popupElement);
+  openPopup(photoPopup);
+}
 
 //создаем заполненную карточку
 function createCard (data) {
   const cardTemplate     = document.querySelector('#card').content;
   const cardElement      = cardTemplate.querySelector('.place').cloneNode(true);
 
-  cardElement.querySelector('.place__image').src=data.link;
-  cardElement.querySelector('.place__text').textContent=data.name;
-  cardElement.querySelector('.place__image').alt=data.name;
+  cardElement.querySelector('.place__image').src = data.link;
+  cardElement.querySelector('.place__text').textContent = data.name;
+  cardElement.querySelector('.place__image').alt = data.name;
 
 
   cardElement.querySelector('.place__button-like').addEventListener('click', function (evt) {
@@ -146,7 +155,9 @@ function createCard (data) {
   });
 
   cardElement.querySelector('.place__image').addEventListener('click', function(evt) {
-    //вызвать функцию открытия попапа большой фотки
+    const elementLink = evt.target.src;
+    const elementName = evt.target.alt;
+    createPopup(elementLink, elementName);
   })
 
   return cardElement;
@@ -157,7 +168,6 @@ function renderCard (card) {
   places.prepend(cardElement);
 }
 
-
 function initialRendeCards () {
   initialCards.forEach(function(card) {
    renderCard(card);
@@ -166,4 +176,9 @@ function initialRendeCards () {
 
 
 initialRendeCards();
+
+
+
+
+
 
