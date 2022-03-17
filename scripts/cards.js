183e08bca1565bc8a6df7625e0770e782c8fb62a
Natalya Serebrennikova <inkinyam@yandex.ru>
2022-03-17 20:24:30 +0300

    сделать корректировки по ревью Я.Практикума

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+// Начальный массив карточек
+const initialCards = [
+  {
+    name: 'Крым',
+    link: 'https://images.unsplash.com/photo-1597181227326-0742677fe67c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Рим',
+    link: 'https://images.unsplash.com/photo-1638707743406-3e2a940252b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
+  },
+  {
+    name: 'Каппадокия',
+    link: 'https://fregataero.ru/images/turkey/Cappadocia/cappadocia-805624_960_720.jpg'
+  },
+  {
+    name: 'Москва',
+    link: 'https://images.unsplash.com/photo-1598516443513-109b9e3f5294?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Стокгольм',
+    link: 'https://images.unsplash.com/photo-1553538435-cb923cf2d82a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://images.unsplash.com/photo-1614093576028-920b30d65326?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
+  }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -25,33 +25,10 @@ const addCardButton    = document.querySelector('.popup-add__save');
 // Контейнер для карточек
 const places            = document.querySelector('.places');
 
-// Начальный массив карточек
-const initialCards = [
-  {
-    name: 'Крым',
-    link: 'https://images.unsplash.com/photo-1597181227326-0742677fe67c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    name: 'Рим',
-    link: 'https://images.unsplash.com/photo-1638707743406-3e2a940252b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-  },
-  {
-    name: 'Каппадокия',
-    link: 'https://fregataero.ru/images/turkey/Cappadocia/cappadocia-805624_960_720.jpg'
-  },
-  {
-    name: 'Москва',
-    link: 'https://images.unsplash.com/photo-1598516443513-109b9e3f5294?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    name: 'Стокгольм',
-    link: 'https://images.unsplash.com/photo-1553538435-cb923cf2d82a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://images.unsplash.com/photo-1614093576028-920b30d65326?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
-  }
-];
+// Переменные для заполнения карточки из попапа
+const cardName = document.querySelector('.popup__item_el_name');
+const cardLink = document.querySelector('.popup__item_el_link');
+
 
 function openPopup(popup){
   popup.classList.add('popup_opened');
@@ -82,8 +59,6 @@ function addFormSubmitHandler (event) {
   const newCard = {
                     name: '',
                     link: ''}
-  let cardName = document.querySelector('.popup__item_el_name');
-  let cardLink = document.querySelector('.popup__item_el_link');
   newCard.name = cardName.value;
   newCard.link = cardLink.value;
 
@@ -151,12 +126,12 @@ function createCard (data) {
   });
 
   cardElement.querySelector('.place__button-delete').addEventListener('click', function (evt) {
-    evt.target.parentElement.remove();
+    evt.target.closest('.place').remove();
   });
 
   cardElement.querySelector('.place__image').addEventListener('click', function(evt) {
-    const elementLink = evt.target.src;
-    const elementName = evt.target.alt;
+    const elementLink = data.link;
+    const elementName = data.name;
     createPopup(elementLink, elementName);
   })
 
@@ -169,11 +144,8 @@ function renderCard (card) {
 }
 
 function initialRendeCards () {
-  initialCards.forEach(function(card) {
-   renderCard(card);
-  })
+  initialCards.forEach(renderCard);
 }
-
 
 initialRendeCards();
 
